4115309f4aca1ff3f341d590da326583728d929c
anxssn <arrzz1207@gmail.com>
2025-12-19 00:21:05 +0700

    lab10

---
+++ laba10/function.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+struct date{
+    int day;
+    int month;
+    int year;
+};
+
+void decade(int day);
+void season(int month);
+void century(int year);
+
+#endif
+
